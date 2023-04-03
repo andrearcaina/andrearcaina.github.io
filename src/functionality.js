9c3092798d0ef4d39ae6ef60aaf3651bff963431
Andre <dtandre331@gmail.com>
2023-04-03 10:04:55 -0400

    Implemented Button and New Project Design
    - implemented button with a side scrolling feature (no responsiveness now)
    - showcased another new design that can be used/implemented/merged into existing project in templates folder
    - moved imgs folder inside template folder
    - imgs are subject to change (might not be for personal/commercial use or is copyrighted)

--- src/functionality.js
+++ src/functionality.js
@@ -1,4 +1,17 @@
-function changeColor(className) {
-    const color = style.getPropertyValue('--after');
-    document.getElementById(className).style.color = color;   
+function scrollToRight() {
+    let aboutMe = document.getElementById('about-info');
+    let contact = document.getElementById('contact-info');
+    let projects = document.getElementById('projects');
+    aboutMe.style.transform = 'translateX(-400%)';
+    contact.style.transform = 'translateX(-400%)';
+    projects.style.transform = 'translateX(0%)';
+}
+
+function scrollToLeft() {
+    let aboutMe = document.getElementById('about-info');
+    let contact = document.getElementById('contact-info');
+    let projects = document.getElementById('projects');
+    aboutMe.style.transform = 'translateX(0%)';
+    contact.style.transform = 'translateX(0%)';
+    projects.style.transform = 'translateX(100%)';
 }
--- template/test.js
+++ template/test.js
@@ -1,3 +1,24 @@
 let slides = document.querySelectorAll('.slide');
 let buttons = document.querySelectorAll('.btn');
 let currentSlide = 1;
+
+// Javascript for image slider manual navigation through clicks
+let manualNav = function(manual){
+    slides.forEach((slide) => {
+    slide.classList.remove('active');
+
+    buttons.forEach((button) => {
+        button.classList.remove('active');
+    });
+    });
+
+    slides[manual].classList.add('active');
+    buttons[manual].classList.add('active');
+}
+
+buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+    });
+});
