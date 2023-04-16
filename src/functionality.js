778d015433d394cbc1c477bbfae642af893bc5d0
Andre <dtandre331@gmail.com>
2023-04-15 20:14:22 -0400

    Implemented light dark mode button
    - added hover effect
    - allowed a change of color (darker blue)

--- src/functionality.js
+++ src/functionality.js
@@ -1,3 +1,28 @@
+function changeColor() {
+    const button = document.getElementById('color-button');
+    const body = document.getElementsByTagName('body')[0];
+    const span = document.querySelector('.green');
+    const bodyStyle = getComputedStyle(body);
+    const backgroundColor = bodyStyle.backgroundColor;
+
+    if (backgroundColor === 'rgb(146, 168, 209)') {
+        body.style.backgroundColor = 'rgb(44, 88, 168)';
+        body.style.color = 'white';
+        button.style.backgroundColor = 'rgb(146, 168, 209)';
+        button.style.color = 'rgb(44, 88, 168)';
+        button.innerHTML = 'light';
+        span.style.color = 'rgb(76,175,80)';
+    }
+    else {
+        body.style.backgroundColor = 'rgb(146, 168, 209)';
+        body.style.color = 'black';
+        button.style.backgroundColor = 'rgb(44, 88, 168)';
+        button.style.color = 'rgb(146, 168, 209)';
+        button.innerHTML = 'dark';
+        span.style.color = 'rgb(40, 139, 40)';
+    }
+}
+
 function scrollToRight() {
     let aboutMe = document.getElementById('about-info');
     let contact = document.getElementById('contact-info');
