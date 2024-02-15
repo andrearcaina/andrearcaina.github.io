45a15d43e68e5bf36276f64eef163d124c3d7f43
Andre <dtandre331@gmail.com>
2024-02-15 15:49:39 -0500

    easter egg in description

--- src/components/homepage/description.jsx
+++ src/components/homepage/description.jsx
@@ -10,21 +10,21 @@ export default function Description() {
 
     return (
         <div>
-            <p className="text-[1.1rem]/[1.75rem] sm:text-lg">
-                what is this? well, i&apos;m glad you asked! the gist is ~
-                a simple website to showcase my <Link href="/projects" className="select-none underline hover:scale-50 hover:text-green-500 duration-300 transition-all">projects</Link> and
+            <div className="text-[1.1rem]/[1.75rem] sm:text-lg">
+                what is this? well, i&apos;m glad you asked! the <Link target="_blank" href="https://gist.github.com/andrearcaina" className="hover:text-green-500 hover:text-[1.25rem]/[1.75rem] cursor-pointer duration-200 transition-all">gist</Link> is ~
+                a simple website to showcase my <Link href="/projects" className="select-none underline hover:animate-pulse hover:text-green-500 duration-300 transition-all">projects</Link> and
                 to learn more about me 
-            </p>
+            </div>
             
             <br />
 
-            <p className="text-[1.1rem]/[1.75rem] sm:text-lg">
+            <div className="text-[1.1rem]/[1.75rem] sm:text-lg">
                 the main thing is ... i love to explore and go on an adventure! if you see
                 me <b className={`cursor-pointer ${hovered ? 'text-green-600 underline' : 'text-green-500'}`} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>afk</b>  
                 {hovered && (
                     <span className="text-sm text-gray-500 ml-1">(away from keyboard)</span>
                 )} i&apos;m usually:
-            </p>
+            </div>
 
             <br />
         </div>
