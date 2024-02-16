712463dbeb24ddecfbfe6377410cf8937132d174
Andre <dtandre331@gmail.com>
2024-02-15 20:19:42 -0500

    styled vim better

--- src/components/aboutpage/vim.jsx
+++ src/components/aboutpage/vim.jsx
@@ -12,24 +12,26 @@ export default function Code() {
             <br />
 
             <div className="bg-black p-4 rounded-lg text-[0.95rem]/[1.75rem]">
-                <p>
-                    a 2nd year co-op student studying computer science at toronto metropolitan university <br />
-                    an IBM z ambassador :D <br />
-                    developing a full stack web application called WizGallery <br />
-                </p>
+                <div>
+                    <p class="text-gray-500">1</p>
+                    <p><span class="text-gray-500">2</span> a 2nd year co-op student studying computer science at Toronto Metropolitan University</p>
+                    <p class="text-gray-500">3</p>
+                    <p><span class="text-gray-500">4</span> an IBM z ambassador :D</p>
+                    <p class="text-gray-500">5</p>
+                    <p><span class="text-gray-500">6</span> developing a full stack web application called WizGallery</p>
+                    <p class="text-gray-500">7</p>
+                </div>
                 
-                <br />
-                
-                <p className="text-blue-500">
-                    ~ <br />
-                    ~ <br />
-                    ~ <br />
-                    ~ <br />
-                    ~ <br />
-                    ~ <br />
-                    ~ <br />
-                    ~
-                </p>
+                <div className="text-blue-500">
+                    <p> ~ </p>
+                    <p> ~ </p>
+                    <p> ~ </p>
+                    <p> ~ </p>
+                    <p> ~ </p>
+                    <p> ~ </p>
+                    <p> ~ </p>
+                    <p> ~ </p>
+                </div>
 
                 <Form Route="aboutme.txt" Command=":q!"/>
             </div>
