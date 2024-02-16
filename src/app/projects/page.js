e5238ffdad6fb1c9f933e89449ac43d65b8e5854
Andre <dtandre331@gmail.com>
2024-02-15 19:30:23 -0500

    currently under construction

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -5,6 +5,7 @@ export default function Projects() {
         <main className="sm:w-[70%] lg:w-[50%] xl:w-[40%] mt-[0.8rem] mb-[2rem] md:mt-[3.5rem] md:mb-[2.5rem] text-white">
             <h1>Welcome to my portfolio</h1>
             <p>this is the project page</p>
+            <p>currently under construction</p>
         </main>
     );
 }
