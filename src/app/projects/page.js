56f9d9f6c726116c95f7ac53916032072281f847
Andre <dtandre331@gmail.com>
2024-02-22 16:59:59 -0500

    'finished' projects

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -32,32 +32,43 @@ export default function Projects() {
             animate={{ opacity: 1, y: 0 }} 
             transition={{ duration: 0.5, delay: 0.2 }}
         >        
+            <p className="text-[1.05rem]/[1.75rem] sm:text-lg text-white mb-4">
+                here are some of my favourite projects that i&apos;ve been currently working on :)
+            </p>
+
             {projects.length > 0 && (
-                <div className="bg-gray-100 p-4 rounded-sm">
+                <div className="bg-gray-400 p-4 rounded-md">
                     <nav className="flex flex-wrap items-center">
                         {projects.map((project) => (
-                            <div key={project.id} className={`cursor-pointer p-2 font-bold ${nav === project.id ? 'text-green-500' : 'hover:text-green-500 duration-300 transition-all'}`} onClick={() => handleNav(project.id)}>
+                            <div key={project.id} className={`cursor-pointer p-2 font-bold ${nav === project.id ? 'text-green-200' : 'hover:text-green-200 duration-300 transition-all'}`} onClick={() => handleNav(project.id)}>
                                 {project.name}
                             </div>
                         ))}
                         
                         <div className="cursor-pointer p-2 font-bold">
-                            <Link target="_blank" href="https://github.com/andrearcaina?tab=repositories" className="underline hover:text-green-500 duration-300 transition-all">more!</Link>
+                            <Link target="_blank" href="https://github.com/andrearcaina?tab=repositories" className="underline hover:text-green-200 duration-300 transition-all">more!</Link>
                         </div>
                     </nav>
-
-                    <Link target="_blank" href={projects[nav].href} className="text-2xl font-bold mb-4 underline hover:text-green-500 hover:text-[1.6rem]/[2rem] duration-300 transition-all">{projects[nav].name}</Link>
-                    
+        
                     <p className="text-md md:text-lg my-5">{projects[nav].info}</p>
                     <p className="text-sm md:text-md mb-4">{projects[nav].description}</p>
-                    <p className="text-sm md:text-md mb-2"> &gt; <span className="font-bold">{projects[nav].technologies}</span></p>
-                    <p className="text-sm md:text-md mb-4"> &gt; <span className="font-bold">{projects[nav].languages}</span></p>
+
+                    <blockquote className="p-4 mb-4 border-l-2 border-gray-600 bg-gray-800 text-sm md:text-md">
+                        <span className="leading-relaxed text-gray-300 font-bold">
+                            <p>&gt; {projects[nav].technologies}</p>
+                            <p>&gt; {projects[nav].languages}</p>
+                        </span>
+                    </blockquote>
                     
-                    {projects[nav].deployment && (
-                        <p className="text-sm md:text-md mb-2">
-                            <Link target="_blank" href={projects[nav].deployment} className="underline hover:text-green-500 duration-300 transition-all">check deployment</Link>
-                        </p>
-                    )}
+                    <div className="flex gap-5">
+                        <Link target="_blank" href={projects[nav].href} className="text-sm md:text-md font-bold mb-4 hover:underline hover:text-green-200 duration-300 transition-all">view source code</Link>
+
+                        {projects[nav].deployment && (
+                            <p className="text-sm md:text-md mb-2">
+                                <Link target="_blank" href={projects[nav].deployment} className="hover:underline hover:text-green-200 duration-300 transition-all">deployed website</Link>
+                            </p>
+                        )}
+                    </div>
                 </div>
             )}
         </motion.main>
