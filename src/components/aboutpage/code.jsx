394030cb5faa4a9b7ea2b8546a9a8f505d56b80c
Andre <dtandre331@gmail.com>
2024-02-23 11:37:07 -0500

    : added

--- src/components/aboutpage/code.jsx
+++ src/components/aboutpage/code.jsx
@@ -4,7 +4,7 @@ import Form from './form';
 export default function Code() {
     return (
         <div className="bg-black p-4 rounded-lg text-[0.95rem]/[1.75rem]">
-            <p><span className="text-green-800">aarcaina@portfolio</span>~$ echo &quot; <br />
+            <p><span className="text-green-800">aarcaina@portfolio</span>:~$ echo &quot; <br />
                 &gt; a 2nd year co-op student studying computer science at <Link className="underline italic hover:text-green-500 duration-300 transition-all" target="_blank"
                     href="https://www.torontomu.ca/" passHref>
                     toronto metropolitan university
@@ -13,7 +13,7 @@ export default function Code() {
 
             <br />
 
-            <p><span className="text-green-800">aarcaina@portfolio</span>~$ echo &quot; <br />
+            <p><span className="text-green-800">aarcaina@portfolio</span>:~$ echo &quot; <br />
                 &gt; an <Link className="underline italic hover:text-green-500 duration-300 transition-all" target="_blank"
                     href="https://www.linkedin.com/posts/andre-arcaina_i-am-now-an-ibm-z-student-ambassador-for-activity-7127723378488082432-fh1F?utm_source=share&utm_medium=member_desktop" passHref>
                     IBM
@@ -22,7 +22,7 @@ export default function Code() {
 
             <br />
             
-            <p><span className="text-green-800">aarcaina@portfolio</span>~$ echo &quot; <br />
+            <p><span className="text-green-800">aarcaina@portfolio</span>:~$ echo &quot; <br />
                 &gt; developing a full stack web application called <Link className="underline italic hover:text-green-500 duration-300 transition-all" target="_blank"
                     href="https://github.com/andrearcaina/WizGallery" passHref>
                     WizGallery
