9d5a7e3b4bb9c75fab9a8e7f35f43fd13c724452
Andre <dtandre331@gmail.com>
2024-04-21 16:03:56 -0400

    new font + project

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,9 @@
 export { default as Footer } from './general/footer';
 export { default as Navbar } from './general/navbar';
 export { default as Main } from './general/main';
+export { default as Body } from './general/body';
+export { default as Display } from './general/display';
+export { default as ThemeButton } from './general/themeButton';
 export { default as Title } from './homepage/title';
 export { default as Blockquote } from './homepage/blockquote';
 export { default as Description } from './homepage/description';
@@ -9,4 +12,3 @@ export { default as Activities } from './homepage/activities';
 export { default as Interests } from './aboutpage/interests';
 export { default as Code } from './aboutpage/code';
 export { default as Vim } from './aboutpage/vim';
-export { default as Display } from './projectpage/display';
